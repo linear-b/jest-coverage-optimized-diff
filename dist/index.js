37e01ce3d484872a02d28c986b06c593de0a1d25
Zuki Sarusi <zuki@linearb.io>
2023-07-31 16:32:19 +0300

    .

--- dist/index.js
+++ dist/index.js
@@ -2053,9 +2053,7 @@ function run() {
                 }
                 return acc;
             }, initialAcc);
-            console.log({ codeCoverageNew });
-            console.log({ codeCoverageOld });
-            console.log({ codeCoverageOld });
+            delete resolvedOld.total;
             const currentDirectory = child_process_1.execSync('pwd')
                 .toString()
                 .trim();
